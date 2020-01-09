b68b397ac0b4cc11a80a018654e16129d549b762
Kevin <bubbagump2020@gmail.com>
2020-01-09 16:02:48 -0600

    Styling coming along nicely. Info panel layout finished

--- periodic/src/components/periodic_table/ElementInfo.js
+++ periodic/src/components/periodic_table/ElementInfo.js
@@ -6,10 +6,10 @@ class ElementInfo extends React.Component{
     render(){
         let element = this.props.element
         return(
-            <div id="information">
-                <h1>{element.name}</h1>
-                <button onClick={this.props.closeInfo}>Close Info</button>
-            </div>
+            <Fragment id="element-box" className={element.category}>
+                <div className="element-number">{element.number}</div>
+                <button id="close-button" onClick={this.props.closeInfo}><strong>X</strong></button>
+            </Fragment>
         )
     }
 }
--- periodic/src/components/periodic_table/Table.js
+++ periodic/src/components/periodic_table/Table.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import './table_styling/Table.css'
 import ElementCard from './ElementCard'
 import { elements } from '../../PeriodicTableJSON.json'
-import ElementInfo from './ElementInfo'
+// import ElementInfo from './ElementInfo'
 
 class Table extends React.Component {
 
@@ -31,12 +31,39 @@ class Table extends React.Component {
     }
 
     render(){
+        let{
+            name,
+            summary,
+            symbol,
+            category,
+            number,
+            source,
+            appearance,
+            atomic_mass,
+            molar_heat,
+            density,
+            melt,
+            boil
+        } = this.state.element
         return(
             <div className="table-wrapper">
                 <h1 id="header">A Fun Periodic Table</h1>
                 <div id="table">
                     {this.createTable()}
-                    {this.state.showInfo ? (<ElementInfo element={this.state.element} closeInfo={this.closeInfo}/>) : ('')}
+                    {this.state.showInfo ? (
+                        <Fragment>
+                            <div id="element-box" className={`${category}`}>
+                                <div className="element-number">{number}</div>
+                                <div className="element-symbol">{symbol}</div>
+                                <div className="element-name">{name}</div>  
+                            </div>
+                            <div id="element-information-box">
+                                <button id="close-button" onClick={this.closeInfo} title="X">X</button>
+                                <h1 className="element-title">{name}</h1>
+                                <div>Appearance: </div>
+                            </div>
+                        </Fragment>
+                    ) : ('')}
                 </div>
             </div>
         )
